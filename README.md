9011d22df358ae67d4b25d33ca6dcc612676bd23
Qiangmao <yeqiangmao@gmail.com>
2022-10-21 20:37:21 +0800

    ”搭建框架“

---
+++ README.md
@@ -0,0 +1,4 @@
+# DcfFrameworkDemo
+
+## 编译
+```gcc DCF_Framework.c  -L ../lib -ldcf  -I ../include```
---
+++ src/DcfFrameworkDemo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "dcf_interface.h"
+
+int main()
+{
+
+    dcf_set_timer(NULL);
+
+    return 0;
+}
