1425a29c29936059c7cb49099fed9fbf81b58393
QiangmaoYe <yeqiangmao@gmail.com>
2022-11-08 01:39:37 +0800

    add read buffer

--- src/DcfFrameworkDemo.c
+++ src/DcfFrameworkDemo.c
@@ -5,8 +5,36 @@
 #include <time.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include "dcf_setparams.h"
-#include "dcf_callbackfunction.h"
+
+typedef struct
+{
+
+    char *buffer;
+    size_t bufferLength;
+    ssize_t inputLength;
+
+} inputBuffer;
+
+inputBuffer *newInputBuffer()
+{
+    inputBuffer *buf = (inputBuffer *)malloc(sizeof(inputBuffer));
+    buf->buffer = NULL;
+    buf->bufferLength = 0;
+    buf->inputLength = 0;
+
+    return buf;
+}
+void readCommand(inputBuffer *buf)
+{
+    ssize_t byteRead = getline(&(buf->buffer), &(buf->bufferLength), stdin);
+    if (byteRead <= 0)
+    {
+        printf("FAIL TO READ COMMAND\n.");
+    }
+
+    buf->inputLength = byteRead - 1;
+    buf->buffer[byteRead - 1] = 0;
+}
 
 void Print_REPL()
 {
@@ -72,7 +100,6 @@ static bool RegisterDcfCallBacks()
     }
     if (dcf_register_election_notify(ElectionCbFunc) != 0)
     {
-
         printf("WARNING,Failed to register PromoteOrDemote.");
         return false;
     }
@@ -95,51 +122,52 @@ bool Set_dcf_param()
         return false;
     }
 
-    // if (dcf_set_param("RUN_MODE", "0") != 0)
-    // {
-    //     return false;
-    // }
+    if (dcf_set_param("RUN_MODE", "0") != 0)
+    {
+        return false;
+    }
 
-    // if (dcf_set_param("FLOW_CONTROL_CPU_THRESHOLD", "100") != 0)
-    // {
-    //     return false;
-    // }
+    if (dcf_set_param("FLOW_CONTROL_CPU_THRESHOLD", "100") != 0)
+    {
+        return false;
+    }
 
-    // if (dcf_set_param("MEC_AGENT_THREAD_NUM", "10") != 0)
-    // {
-    //     return false;
-    // }
+    if (dcf_set_param("MEC_AGENT_THREAD_NUM", "10") != 0)
+    {
+        return false;
+    }
 
-    // if (dcf_set_param("MEC_REACTOR_THREAD_NUM", "1") != 0)
-    // {
-    //     return false;
-    // }
- 
-    // if (dcf_set_param("CONNECT_TIMEOUT", "6000") != 0)
-    // {
-    //     return false;
-    // }
-    // if (dcf_set_param("REP_APPEND_THREAD_NUM", "2") != 0)
-    // {
-    //     return false;
-    // }
+    if (dcf_set_param("MEC_REACTOR_THREAD_NUM", "1") != 0)
+    {
+        return false;
+    }
+
+    if (dcf_set_param("CONNECT_TIMEOUT", "6000") != 0)
+    {
+        return false;
+    }
+    if (dcf_set_param("REP_APPEND_THREAD_NUM", "2") != 0)
+    {
+        return false;
+    }
+    // Can't set MEC_FRAGMENT_NUM
     // if (dcf_set_param("MEC_FRAGMENT_NUM", "64") != 0)
     // {
     //     return false;
     // }
-    // if (dcf_set_param("STG_POOL_INIT_SIZE", "32") != 0)
-    // {
-    //     return false;
-    // }
-    // if (dcf_set_param("STG_POOL_MAX_SIZE", "2048") != 0)
-    // {
-    //     return false;
-    // }
+    if (dcf_set_param("STG_POOL_INIT_SIZE", "32") != 0)
+    {
+        return false;
+    }
+    if (dcf_set_param("STG_POOL_MAX_SIZE", "2048") != 0)
+    {
+        return false;
+    }
 
-    // if (dcf_set_param("MEC_POOL_MAX_SIZE", "200") != 0)
-    // {
-    //     return false;
-    // }
+    if (dcf_set_param("MEC_POOL_MAX_SIZE", "200") != 0)
+    {
+        return false;
+    }
     if (dcf_set_param("DATA_PATH", "../primary_data_dir/dcf_data") != 0)
     {
         return false;
@@ -155,13 +183,31 @@ bool Set_dcf_param()
         return false;
     }
 
+    if (dcf_set_param("SSL_CIPHER", "ECDHE-ECDSA-AES256-GCM-SHA384:") != 0)
+    {
+        return false;
+    }
+
     return true;
 }
 
-void Exit_Dcf_Framework()
+void init_dcf_SSL()
 {
+}
 
-    dcf_stop();
+void Exit_Dcf(inputBuffer *buf)
+{
+    free(buf);
+    free(buf->buffer);
+    if (dcf_stop() != 0)
+    {
+        printf("Exit Dcf fail.\n");
+    }
+    else
+    {
+        printf("Dcf Stop.Bye.\n");
+        exit(EXIT_SUCCESS);
+    }
 }
 
 char buf[1024] = {'\0'};
@@ -179,6 +225,7 @@ char *Read_Dcf_start_Config()
 
     return buf;
 }
+
 int main(int argc, char *argv[])
 { // Read_Buffer is designed for dcf_read.
     char *Read_Buffer;
@@ -217,6 +264,7 @@ int main(int argc, char *argv[])
         {
             is_start_dcf = true;
             printf("DCF START SUCCESS!\n");
+            printf("\n\n");
             break;
         }
     }
@@ -224,21 +272,27 @@ int main(int argc, char *argv[])
 
     {
         printf("WARNING,CAN NOT START DCF IN %.2fs.\n", duration);
-
-        printf("\n");
+        exit(EXIT_FAILURE);
     }
-
+    inputBuffer *input_buffer = newInputBuffer();
+    int Command_buffer;
     Print_REPL();
     // 测试框架
-    // do
-    // {
-    //     Print_Prompt();
-    //     printf("hello\n");
-    //     scanf("%s", &Command_buffer);
-    //     dcf_write();
-    //     // // dcf_read(1,index,Read_Buffer,length);
-    //     // printf("%s\n", Read_Buffer);
-    // } while (true);
+    do
+    {
+        Print_Prompt();
+        readCommand(input_buffer);
+        if (strcmp(input_buffer->buffer, ".exit") == 0)
+        {
+            Exit_Dcf(input_buffer);
+        }
+        // if (dcf_write(1,) != 0)
+        // {
+        //     printf("dcf write fail.\n");
+        // }
+        // // dcf_read(1,index,Read_Buffer,length);
+        printf("%s\n", input_buffer->buffer);
+    } while (true);
 
     return 0;
 }
